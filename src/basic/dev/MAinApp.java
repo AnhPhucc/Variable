cb88ac7bf43720da136342131a160d74890cadd7
AnhPhucc <nvapcsnat@gmail.com>
2021-10-20 13:33:06 +0700

    first commit

---
+++ src/basic/dev/MAinApp.java
@@ -0,0 +1,26 @@
+package phuc.dev;
+/**
+ * 
+ * @author AnhPhuc
+ *
+ */
+
+public class MAinApp {
+	public static void main (String[] args) {
+		/**
+		 * khai bao bien
+		 */
+		String firstName = "Nguyen";
+		String lastName = "Phuc";
+		int old = 19;
+		float markMath = 9f;
+		boolean passed = true;
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + " "+ lastName);
+		System.out.println("Passed: " + passed);
+		
+		//khai bao hang. Hang phai viet hoa
+		final float PI = 3.14f;
+	}
+
+}
